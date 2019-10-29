38addb983fa37483d137ac7d75b4cf09fe031ef7
Brian1011 <mutindabrian26@gmail.com>
2019-10-29 12:21:51 +0300

    get university by location

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import hello.wsdl.GetAllUniversitiesResponse;
+import hello.wsdl.GetUniversityByLocationResponse;
 import hello.wsdl.GetUniversityResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +18,7 @@ public class DemoApplication {
     @Bean
     CommandLineRunner lookup(UniversityClient quoteClient){
         return args -> {
+            // get university by name
             String name = "Strathmore";
 
             if(args.length > 0 ){
@@ -24,8 +26,27 @@ public class DemoApplication {
             }
 
             GetUniversityResponse response = quoteClient.getUniversity(name);
-            //System.err.println(response.getUniversity().getName());
-            System.err.println(response.getUniversity().getName());
+            System.err.println("############### BY NAME ##################");
+            System.err.println("University Name: "+response.getUniversity().getName()
+                    + " Location: "+response.getUniversity().getLocation()
+                    + " Year Founded: "+response.getUniversity().getYearFounded()
+            );
+
+
+            // get university by location
+            String location = "Juja";
+
+            if (args.length > 0 ){
+                location = args[0];
+            }
+
+            GetUniversityByLocationResponse response1 = quoteClient.getUniversityByLocationRequest(location);
+            System.err.println("############### BY LOCATION ##################");
+            System.err.println("University Name: "+response1.getUniversity().getName()
+                    + " Location: "+response1.getUniversity().getLocation()
+                    + " Year Founded: "+response1.getUniversity().getYearFounded()
+            );
+
         };
     }
 
--- src/main/java/com/example/demo/UniversityClient.java
+++ src/main/java/com/example/demo/UniversityClient.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import hello.wsdl.GetUniversityByLocationRequest;
+import hello.wsdl.GetUniversityByLocationResponse;
 import hello.wsdl.GetUniversityRequest;
 import hello.wsdl.GetUniversityResponse;
 import org.slf4j.LoggerFactory;
@@ -10,17 +12,32 @@ import org.springframework.ws.soap.client.core.SoapActionCallback;
 public class UniversityClient extends WebServiceGatewaySupport {
     private static final Logger log = LoggerFactory.getLogger(UniversityClient.class);
 
+    // get university by name
     public GetUniversityResponse getUniversity(String name){
         GetUniversityRequest request = new GetUniversityRequest();
         request.setName(name);
 
-        log.info("Requesting name for "+name);
+        log.info("Requesting univerity by name - "+name);
 
         GetUniversityResponse response = (GetUniversityResponse) getWebServiceTemplate()
                 .marshalSendAndReceive("http://localhost:7991/ws/universities", request,
                             new SoapActionCallback("http://spring.io/guides/gs-producing-web-service/GetCountryRequest")
                         );
 
+        return response;
+    }
+
+    // get university by location
+    public GetUniversityByLocationResponse getUniversityByLocationRequest(String location){
+        GetUniversityByLocationRequest request = new GetUniversityByLocationRequest();
+        request.setLocation(location);
+
+        log.info("Requesting University based on location - "+location);
+
+        GetUniversityByLocationResponse response = (GetUniversityByLocationResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:7991/ws/universities", request,
+                            new SoapActionCallback("http://spring.io/guides/gs-producing-web-service/GetCountryRequest")
+                        );
         return response;
     }
 }
