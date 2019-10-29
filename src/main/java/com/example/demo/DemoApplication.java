1e2d13a3ec09a34fe46f2b9ea57ea080b57e4a55
Brian1011 <mutindabrian26@gmail.com>
2019-10-29 12:00:36 +0300

    get university y name

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import hello.wsdl.GetAllUniversitiesResponse;
+import hello.wsdl.GetUniversityResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner lookup(UniversityClient quoteClient){
+        return args -> {
+            String name = "Strathmore";
+
+            if(args.length > 0 ){
+                name = args[0];
+            }
+
+            GetUniversityResponse response = quoteClient.getUniversity(name);
+            //System.err.println(response.getUniversity().getName());
+            System.err.println(response.getUniversity().getName());
+        };
     }
 
 }
--- src/main/java/com/example/demo/UniversityClient.java
+++ src/main/java/com/example/demo/UniversityClient.java
@@ -1,6 +1,26 @@
 package com.example.demo;
 
+import hello.wsdl.GetUniversityRequest;
+import hello.wsdl.GetUniversityResponse;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class UniversityClient extends WebServiceGatewaySupport {
+    private static final Logger log = LoggerFactory.getLogger(UniversityClient.class);
+
+    public GetUniversityResponse getUniversity(String name){
+        GetUniversityRequest request = new GetUniversityRequest();
+        request.setName(name);
+
+        log.info("Requesting name for "+name);
+
+        GetUniversityResponse response = (GetUniversityResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:7991/ws/universities", request,
+                            new SoapActionCallback("http://spring.io/guides/gs-producing-web-service/GetCountryRequest")
+                        );
+
+        return response;
+    }
 }
